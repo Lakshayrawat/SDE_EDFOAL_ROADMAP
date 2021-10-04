f169a2846ffc08ff9b6bebbfb32d14e70ef72a32
lakshay rawat <65499159+Lakshayrawat@users.noreply.github.com>
2021-10-04 20:52:56 +0530

    Update Remove_element.java

--- Remove_element.java
+++ Remove_element.java
@@ -1,3 +1,13 @@
+//APPROACH
+/*To do this problem in O(1) space we use the following approach. In this, first we take input array from user the value that we want to remove. 
+After taking the input,we start iterating on the input array and also make a pointer variable that point on the starting element of the array.
+Now, while iterating it will check the 'i' element is equal to the val(value to be removed) or not.
+If not it will simply put element present at 'j' position to the 'i' and increment j.
+But if it is equal then it will not put the 'i' element to 'j' and not increment 'j'.
+After doing the full iteration. It will print the element less than jth index.
+
+Basically in this approach we are just replacing the val(value to be removed) with value which is not to be removed.
+
 package array;
 import java.util.*;
 
