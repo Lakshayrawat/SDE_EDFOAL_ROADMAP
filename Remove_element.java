851f96e77d52001a2c35f386fb87995da5914a82
lakshay rawat <65499159+Lakshayrawat@users.noreply.github.com>
2021-10-04 13:38:48 +0530

    Add files via upload

---
+++ Remove_element.java
@@ -0,0 +1,35 @@
+package array;
+import java.util.*;
+
+public class Remove_element {
+	
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size of array");
+		int n = sc.nextInt();
+		
+		int arr[] = new int[n];
+		
+		System.out.println("Enter array");
+		for(int i = 0; i < n; i++) {
+			
+			arr[i] = sc.nextInt();
+			
+		}
+		
+		System.out.println("Enter value to be remove from array");
+		int val = sc.nextInt();
+		int j = 0;
+		for(int i = 0; i < n; i++) {
+			if(arr[i]!=val) {
+				arr[j]=arr[i];
+				j++;
+			}
+		}
+		for(int i = 0; i < j; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
+}
