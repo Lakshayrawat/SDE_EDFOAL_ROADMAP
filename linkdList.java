ff76f9145eb78da332a5b682bbc4d02c448a75e2
lakshay rawat <65499159+Lakshayrawat@users.noreply.github.com>
2021-10-19 22:55:16 +0530

    Update linkdList.java

--- linkdList.java
+++ linkdList.java
@@ -1,153 +1,133 @@
-package LinkedList;
+package linkedList;
 
-public class linkdList {
+class Node{
+	int data;
+	Node next;
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+}
 
-	Node head;
+public class LinkedList {
+
+	Node head = null;
 	
 	
-	//This function finds size of the linked list;
+
+//1.Linked List Insertion 
 	
-	int size() {
-		Node n=head;
-		int c=1;
-		if(head==null) {
-			return 0;
+	public Node insert(int data) {
+		
+		Node newNode = new Node(data);
+		
+		if(head == null) {
+			head = newNode;
+		
 		}
 		else {
-			while(n.next!=null) {
-				n=n.next;
-				c++;
+			Node p = head;
+			
+			while(p.next != null) {
+				p = p.next;
 			}
-			return c;
+			p.next = newNode;
+			
+		}
+		
+		return head;
+	}
+//Displaying the LinkedList	
+	public void print() {
+		Node node = head;
+		while(node != null) {
+			System.out.print(node.data+" ");
 		}
 	}
 	
-	//This function create a new node and insert it in the linked list;
-	
-	void insert(int data) {
-		Node node = new Node();
-		node.data=data;
-		node.next=null;
-		
-		if(head==null) {
-			head=node;
-		}
-		else
-		{
-			Node n=head;
-			while(n.next!=null) {
-				n=n.next;
-			}
-			n.next = node;
-		}
-	}
-	
-	//This is used to print the linked list;
-	
-	void show() {
-		Node n=head;
-		while(n.next!=null) {
-			System.out.println(n.data);
-			n=n.next;
-		}
-		System.out.println(n.data);	
-		}
-	
-	//This is used to insert node in the starting or we can say that add new node as head node;
-	
-	void insertAtstart(int data) {
-		
-		Node node  = new Node();
-		node.data=data;
-		node.next=head;
-		head=node;
-	}
-	
-	//This is used to insert node at a particular index;
-	
-	void insertAt(int index, int data) {
-		Node node = new Node();
-		node.data = data;
-	
-		int size = size();
-		if(index<0 || size<index) {
-			System.out.println("Invalid index");
-			return;
-		}
-		
-		else if(index==0) {
-			node.next =head;
-			head = node;
+//2. Linked List Deletion (Deleting a given key)
+	public Node deleteKey(Node node) {
+		if(node == head) {
+			head = head.next;
 		}
 		else {
-			Node n =head;
-			for (int i = 0; i < index-1; i++) {
-				n=n.next;
-			}
-			node.next =n.next;
-			n.next=node;
-		}
-		
-		}
-	
-	
-	//This is used to delete the node presented at the provided index;
-	
-		void deleteAt(int index) {
-		
-		int size = size();
-		if(index<0 || size<index) 
-		{
-			System.out.println("Invalid index");
-			return;
-		}
-			
-			
-		else if(index==0) {
-			head=head.next;
-		}
-		else {
-		Node n= head;
-		for (int i = 0; i < index-1; i++) {
-			n = n.next;
-		}
-		n.next=n.next.next;
-		}
-		
-		}
-	
-		//This is delete function does not take index to delete but delete identifying data;
-		
-		void deletedata(int data) {
-			
-			if(head.data==data) {
-				head=head.next;
-			}
-			else {
-				Node n=head;
-				while(n.next.data!=data) {
-					n=n.next;
-				}
-				n.next=n.next.next;
-			}
-			
-		}
-		
-		Node reverse() {
-			
 			Node prev = null;
-			Node current = head;
-			Node next = null;
-			while(current!= null) {
-				
-				    next = current.next;
-		            current.next = prev;
-		            prev = current;
-		            current = next;
-		            
+			Node p = head;
+			while(p != null && p != node) {
+				prev = p;
+				p = p.next;
 			}
-			return prev;
+			prev.next = p.next;
 		}
+		
+		return head;
+	}
+//3. Linked List Deletion (Deleting a key at given position)
+	public void deletePos(int index) {
+		if(index == 0) {
+			head = head.next;
+		}
+		int count = 0;
+		Node p = head;
+		
+		while(count != index-1) {
+			count++;
+			p = p.next;
+		}
+		p.next = p.next.next;
+	}
+
+//5. Find Length of a Linked List (Iterative and Recursive)
+	public int len(Node node) {
+		int size = 0;
+		Node p = head;
+		while(p!=null) {
+			size++;
+			p = p.next;
+		}
+		return size;
+	}
+//6. Search an element in a Linked List (Iterative and Recursive)
+	//Iterative
+	public void  search(Node node) {
+		Node p = head;
+		while(p!=null) {
+			if(p == node) {
+				System.out.println("Found");
+				return;
+			}
+			p = p.next;
+		}
+		System.out.println("Not found");
+	}
+	//Recursive
+	public void  searchRecurse(Node node) {
+		Node p = head;
+		if(p == null) {
+			System.out.println("Not found");
+			return;
+		}
+		if(p == node) {
+			System.out.println("Found");
+			return;
+		}
+		
+		searchRecurse(node.next);
+		
+	}
+	
+//7. Write a function to get Nth node in a Linked List
+
+	public Node nthNode(int n) {
+		int count = 0;
+		Node p = head;
+		while(p != null && count != n) {
+			count++;
+			p = p.next;
+		}
+		
+		return p;
+	}
 	
 	
 }
